38203e2d46d7dd6e7973a5203ab94e596247692b
isyamarshabindarputra <isyabindarp@gmail.com>
2025-11-17 10:13:39 +0700

    perbaiki tampilan

--- resources/views/karyawans/create.blade.php
+++ resources/views/karyawans/create.blade.php
@@ -40,7 +40,7 @@
 
         <div class="mb-6">
             <label for="bidang" class="block text-gray-700 mb-2">Bidang</label>
-            <select name="bidang" class="form-control" required>
+            <select name="bidang" id="bidang" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required> required>
                 <option value="Sekre">Sekre</option>
                 <option value="TIK">TIK</option>
                 <option value="Stasan">Stasan</option>
